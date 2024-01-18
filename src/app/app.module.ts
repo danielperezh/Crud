2507bc2ec3a8127ad64fc2aa25b04b7458395636
Daniel Perez <daniel.perezh@usantoto.edu.co>
2024-01-18 16:23:21 -0500

    Actualizacion 18/01/2024

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
+import { CuerpoComponent } from './componentes/cuerpo/cuerpo.component';
+
+import { FormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    CuerpoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ToastrModule.forRoot(),
+    ModalModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
